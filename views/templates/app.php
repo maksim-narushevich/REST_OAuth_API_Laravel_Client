797d6467665e38ee0c8aa9e766d35322e5b44ef8
maxim.narushevich <maxi@digitalmind.be>
2018-04-25 10:56:39 +0300

    - Added functionality for Get-Token page - Added Bootstrap grid - Improved layout

--- views/templates/app.php
+++ views/templates/app.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     {% block head %}
-    <link rel="stylesheet" href="/public/css/style.css" />
+    <link rel="stylesheet" href="/public/css/style.css"/>
     <title>{% block title %}{% endblock %} - OAuth REST Client</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -12,12 +12,21 @@
     {% endblock %}
 </head>
 <body>
-<div id="content">{% block content %}{% endblock %}</div>
-<div id="footer">
-    {% block footer %}
-    &copy; Copyright 2011 by <a href="http://domain.invalid/">you</a>.
-    {% endblock %}
+<div class="row">
+    <div class="col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2">
+
+
+        <div id="content">{% block content %}{% endblock %}</div>
+        <div class="col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2">
+            <div id="footer">
+                {% block footer %}
+                Developed by <a href="https://www.linkedin.com/in/maksim-narushevich-b99783106/" target="_blank">Maksim Narushevich</a>.
+                {% endblock %}
+            </div>
+        </div>
+    </div>
 </div>
+
 <script src="/public/js/script.js"></script>
 </body>
 </html>
--- views/templates/token.php
+++ views/templates/token.php
@@ -3,29 +3,92 @@
 {% block head %}
 {{ parent() }}
 <style type="text/css">
-    .important { color: #336699; }
+    #token_value {
+        word-wrap: break-word;
+    }
 </style>
 {% endblock %}
 {% block content %}
-<h1>Index</h1>
-<p class="important">
-    Welcome to Token page.
-</p>
+
+
+<div class="col-sm-12 col-xs-12">
+<div class="col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2">
+    <div id="token_form">
+    <h1>Get token</h1>
+        <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" id="email">
+            <span id="email_error" style="color: red;display: none;"></span>
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd">
+            <span id="pwd_error" style="color: red;display: none;"></span>
+        </div>
+        <button class="btn btn-success" id="getToken">Get token</button>
+    </div>
+    <div class="form-group" id="token_value">
+
+    </div>
+    <div id="token_button_try_again" style="display: none;">
+        <button  class="btn btn-warning" id="tryAgain">Try again</button>
+    </div>
+</div>
+</div>
+
 
 
 <script>
-    $( document ).ready(function() {
-        $.ajax({
-            method: 'POST',
-            url: '/test_ajax',
-            data: {
-                strEmail:'anna@gmail.com',
-                strPassword:'annaqwerty'
-            },
-            success: function (data) {
-                console.log(data);
+    $("#getToken").click(function () {
+        var strEmail=$('#email').val();
+        var strPass=$('#pwd').val();
+        $('#token_value').html("");
+        $('#email_error,#pwd_error').hide();
+
+        var blnConfirm = confirm("Are you sure that inserted data is correct?");
+        if (blnConfirm == true) {
+            var ajaxStatus=true;
+
+            if( !strEmail) {
+                ajaxStatus=false;
+                $('#email_error').css('display', 'inline');
+                $('#email_error').html("* Email field can not be empty!");
             }
-        });
+
+            if( !strPass) {
+                ajaxStatus=false;
+                $('#pwd_error').css('display', 'inline');
+                $('#pwd_error').html("* Password field can not be empty!");
+            }
+
+            if(ajaxStatus) {
+                $.ajax({
+                    method: 'POST',
+                    url: '/test_ajax',
+                    data: {
+                        strEmail: strEmail,
+                        strPassword: strPass
+                    },
+                    success: function (data) {
+                        $('#token_form,#email_error,#pwd_error').hide();
+                        $('#token_button_try_again').css('display', 'inline-block');
+                        if (!data['error']) {
+                            $('#token_value').html("<h1 style='text-weight:bold;'>This is your access token:</h1><p >" + data['result'] + "</p>");
+                        } else {
+                            $('#token_value').html("<p style='color:red;' class='w3-center'><img style='margin-top: 25px;'  height='200' src='http://www.heaven4netent.com/wp-content/uploads/2015/10/Sorry.jpg'><br>Provided login or password is not correct!</p>");
+                        }
+
+                    }
+                });
+            }
+        }
+    });
+    $("#tryAgain").click(function () {
+       $('#email,#pwd').val("");
+        $('#token_form').show();
+        $('#token_value').html("");
+        $('#email_error,#pwd_error').hide();
+        $('#token_button_try_again').css('display','none');
     });
 </script>
 {% endblock %}
