b12555ccde72345ff92eda03777659e217830a27
maxim.narushevich <maxi@digitalmind.be>
2018-04-25 11:08:03 +0300

    - Added home page links and changed layout

--- views/templates/home.php
+++ views/templates/home.php
@@ -3,12 +3,20 @@
 {% block head %}
 {{ parent() }}
 <style type="text/css">
-    .important { color: #336699; }
+    #links a { margin-bottom: 15px;
+    display: inline-block;}
 </style>
 {% endblock %}
 {% block content %}
 <h1>Home</h1>
-<p class="important">
-    This is home page!
-</p>
+<div class="col-sm-12 col-xs-12">
+    <div class="col-sm-6  col-xs-12" id="links">
+        <a href="/get-token" >Get authorization token</a><br>
+
+        <a href="/register" >Register new user</a><br>
+
+        <a href="/test" >Try API playground</a><br>
+        <a href="/test" >Check API documentation based on Swagger</a><br>
+    </div>
+    </div>
 {% endblock %}
